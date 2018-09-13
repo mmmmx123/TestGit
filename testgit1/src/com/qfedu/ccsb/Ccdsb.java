4676d24af46c7d3970e01bd870d8b2746dfd899d
mmmmx123 <43225612+mmmmx123@users.noreply.github.com>
2018-09-13 15:40:20 +0800

    update name

--- testgit1/src/com/qfedu/ccsb/Ccdsb.java
+++ testgit1/src/com/qfedu/ccsb/Ccdsb.java
@@ -5,6 +5,6 @@ public class Ccdsb {
 	public static void main(String[] args) {
 		System.out.println("CC你真skr弟弟");
 		System.out.println("笑skr人了");
-		System.out.println("嘻嘻嘻");
+		System.out.println("嘿嘿嘿");
 	}
 }
