9d42955ada1031a54cb9a486d654e58d7b52d39b
c981393350 <43226075+c981393350@users.noreply.github.com>
2018-09-13 15:50:57 +0800

    Update Ccdsb.java

--- testgit1/src/com/qfedu/ccsb/Ccdsb.java
+++ testgit1/src/com/qfedu/ccsb/Ccdsb.java
@@ -3,7 +3,7 @@ package com.qfedu.ccsb;
 public class Ccdsb {
 
 	public static void main(String[] args) {
-		System.out.println("CC你真skr弟弟");
+		System.out.println("CC你真牛逼，我甘拜下风");
 		System.out.println("笑skr人了");
 		System.out.println("嘿嘿嘿");
 	}
